0b76209167a4f4e8e2ff154e3d15ff8f11242a05
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2018-10-01 23:44:04 +0300

    Add test example

---
+++ src/main/java/acme/tests/chapter5/test21/Example.java
@@ -0,0 +1,13 @@
+package acme.tests.chapter5.test21;
+
+public class Example {
+    public static void main(String[] args) {
+        do {
+            System.out.println("helium");
+        } while (false);
+
+        do
+            System.out.println("helium");
+        while (false);
+    }
+}
