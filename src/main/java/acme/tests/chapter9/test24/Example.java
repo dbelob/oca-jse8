553ae1348b41fc0f20582d9c694ec9d2cca22d3f
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2018-10-25 20:00:05 +0300

    Add test examples

---
+++ src/main/java/acme/tests/chapter9/test24/Example.java
@@ -0,0 +1,9 @@
+package acme.tests.chapter9.test24;
+
+public class Example {
+    public static void secret(String mystery) {
+        mystery = mystery.replace("1", "8");
+        mystery.startsWith("paper");
+        String s = mystery.toString();
+    }
+}
---
+++ src/main/java/acme/tests/chapter9/test25/Example.java
@@ -0,0 +1,13 @@
+package acme.tests.chapter9.test25;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Example {
+    public static void main(String[] args) {
+        List balloons = new ArrayList();
+//        List<> balloons1 = new ArrayList();
+        List balloons2 = new ArrayList<>();
+//        List<> balloons3 = new ArrayList<>();
+    }
+}
