bb646d6ae4115af02a02bf6a362a905d549212ae
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2018-11-06 20:57:11 +0300

    Add chapter examples

---
+++ src/main/java/acme/chapter3/creating_dates_and_times/Example.java
@@ -0,0 +1,13 @@
+package acme.chapter3.creating_dates_and_times;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class Example {
+    public static void main(String[] args) {
+        System.out.println(LocalDate.now());
+        System.out.println(LocalTime.now());
+        System.out.println(LocalDateTime.now());
+    }
+}
