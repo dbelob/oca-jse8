3995019ed5e612cd54886af882be5a231bf83a41
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2018-11-20 20:27:49 +0300

    Add test examples

---
+++ src/main/java/acme/tests/chapter10/test79/Bear.java
@@ -0,0 +1,11 @@
+package acme.tests.chapter10.test79;
+
+abstract class Bear {
+    protected int sing;
+
+    protected abstract int grunt();
+
+    int sing() {
+        return sing;
+    }
+}
---
+++ src/main/java/acme/tests/chapter10/test79/PolarBear.java
@@ -0,0 +1,13 @@
+package acme.tests.chapter10.test79;
+
+public class PolarBear extends Bear {
+    @Override
+    protected int grunt() {
+        return 0;
+    }
+//    int grunt() {
+//        sing() += 10;
+//        return super.grunt() + 1;
+//        return 10;
+//    }
+}
