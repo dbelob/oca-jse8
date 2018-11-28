6eee5397ed0e06c73a80bb05cc56e5b2b25e3edc
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2018-11-28 20:07:46 +0300

    Add test examples

--- src/main/java/acme/enthuware/standard_tests/test2/test28/Example.java
+++ src/main/java/acme/enthuware/standard_tests/test2/test28/Example.java
@@ -2,7 +2,10 @@ package acme.enthuware.standard_tests.test2.test28;
 
 public class Example {
     public static void main(String[] args) {
-        System.out.println("new Boolean(\"false\") == false: " + (new Boolean("false") == false));
-        System.out.println("new Boolean(\"true\") == true:   " + (new Boolean("true") == true));
+        System.out.println("new Boolean(\"false\") == false:      " + (new Boolean("false") == false));
+        System.out.println("new Boolean(\"true\") == true:        " + (new Boolean("true") == true));
+        System.out.println();
+        System.out.println("new Integer(42) == 42:              " + (new Integer(42) == 42));
+        System.out.println("new Integer(42) == new Integer(42): " + (new Integer(42) == new Integer(42)));
     }
 }
